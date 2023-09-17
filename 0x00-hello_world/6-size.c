03857b32aa278ec9432623a78bbf4e63fcfaa73e
MbauniJoseph <mbauni.wambugu22@students.dkut.ac.ke>
2023-09-16 23:19:33 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,10 @@
  *Return: o on success
  */
 int main(void)
+{
 	printf("Size of a char: %lu byte(s)", sizeof(char));
 	printf("Size of int: %lu bytes(s)", sizeof(int));
 	printf("size of a long int: %lu bytes(s)", sizeof (long long int));
 	printf("size of a float: %lu byte(s)", sizeof(float));
-	retur (0);
+	return (0);
 }
