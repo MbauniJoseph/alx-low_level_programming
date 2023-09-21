46e358fd5ac999b3806521087369a83c5172150e
MbauniJoseph <mbauni.wambugu22@students.dkut.ac.ke>
2023-09-21 07:00:12 -0700

    task 1 done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  *  * main - tests the function
- *   *   * Return: 0
- *    *    */
+ * main - tests the function
+ * Return: 0
+ */
 
 int main(void)
 {
 	int i;
 
-	i = 0 ;
+	i = 0;
 	positive_or_negative(i);
 
-	Return (0);
+	Return(0);
 }
