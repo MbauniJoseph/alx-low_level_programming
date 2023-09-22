76c2edc17f374207d1704e1e5aef2c0494a265db
MbauniJoseph <mbauni.wambugu22@students.dkut.ac.ke>
2023-09-22 06:21:25 -0700

    1isadigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: character number
+ * _isupper - uppercase letters
+ * @c: char to check
  *
- * Return: 1 if Uppercase, else 0
+ * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
+
+
